114667353022a3055fcae81324efc6f5ee9ade50
Richie Arnold <richard@ambercouch.co.uk>
2024-06-12 13:20:10 +0100

    CFA - Theme style updates including banner, widget titles and footer

--- templates/snippets/site__navigation--main.php
+++ templates/snippets/site__navigation--main.php
@@ -45,16 +45,23 @@
                 <span class="branding__description"><?php bloginfo('description'); ?></span>
               <?php endif; ?>
               <div class="header--master__lang-switch">
+                <div class="social-nav--header">
+                  <a href="tel:02920382671" target="_blank" class="a--icon a--social-nav"><svg class="icon icon-phone icon--header"><use xlink:href="#icon-phone"></use></svg><span class="c-link__lable--tel">029 2038 2671</span></a>
+                  <a href="https://www.facebook.com/cathedraldentalclinic" target="_blank" class="a--icon"><svg class="icon icon-facebook2 icon--header"><use xlink:href="#icon-facebook2"></use></svg></a>
+
+                  <a href="https://www.instagram.com/cathedraldentalclinic166/" target="_blank" class="a--icon"><svg class="icon icon-instagram icon--header"><use xlink:href="#icon-instagram"></use></svg></a>
+
+                </div>
                 <?php echo  langSwitch(); ?>
               </div>
-              <div class="header--master__tel">
-                <a href="tel:02920382671">
-                  <svg preserveAspectRatio="none" class="icon header--master__tel__call ">
-                  <use xlink:href="<?php //echo '/content/themes/ac-inuk/assets/images/defs.svg';                       ?>#icon-call" />
-                  </svg>
-                  029 2038 2671
-                </a>
-              </div>
+<!--              <div class="header--master__tel">-->
+<!--                <a href="tel:02920382671">-->
+<!--                  <svg preserveAspectRatio="none" class="icon header--master__tel__call ">-->
+<!--                  <use xlink:href="#icon-call" />-->
+<!--                  </svg>-->
+<!--                  029 2038 2671-->
+<!--                </a>-->
+<!--              </div>-->
             </div><!-- /.branding -->
             <a id="site_menu_toggle" class="menu--responsive-toggle__toggle" href="#menu">Menu</a>
             <?php wp_nav_menu(array('theme_location' => 'primary', 'container' => 'nav', 'container_class' => 'menu--site__container', 'menu_class' => 'menu--site__menu-list')); ?>
